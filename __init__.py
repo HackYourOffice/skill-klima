62ca896005bd39a2e4291209e336d949014dd295
Daniel Fuchs <dfuchs@synyx.de>
2018-11-23 23:42:55 +0100

    add set temperature

--- __init__.py
+++ __init__.py
@@ -21,5 +21,11 @@ class KlimaSkill(MycroftSkill):
         temperature = r.json()["state"]
         self.speak_dialog("getTemperature", data={"temperature": temperature})
 
+        @intent_handler(IntentBuilder("SetTemperaturIntent").require("setTemperature").require("temperature").build())
+    def handle_get_temperature_intent(self, message):
+        print(message.data)
+        temperature = message.data.get("temperature")
+        self.speak_dialog("setTemperature", data={"temperature": temperature})
+
 def create_skill():
     return KlimaSkill()
