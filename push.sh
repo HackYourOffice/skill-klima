d46b7d20704fe73696c1c50213397bf2c6512f70
Daniel Fuchs <dfuchs@synyx.de>
2018-11-23 23:01:38 +0100

    first try for set temperature

--- push.sh
+++ push.sh
@@ -3,9 +3,9 @@ git commit -m "first try for set temperature"
 git push origin master 
 cd ../mycroft-skills
 git pull
-git submodule update --remote skill-$skill
-git add skill-$skill
-git commit -m "update $skill"
+git submodule update --remote skill-$1
+git add skill-$1
+git commit -m "update $1"
 git push origin 18.08
 cd ../skill-klima
-ssh mycorf@10.0.200.216 './mycroft-core/bin/mycroft-msm -u https://github.com/HackYourOffice/mycroft-skills update skill-$skill'
+ssh mycorf@10.0.200.216 './mycroft-core/bin/mycroft-msm -u https://github.com/HackYourOffice/mycroft-skills update skill-$1'
