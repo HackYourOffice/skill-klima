377563800e6aaba789b55554f7746edd83497779
Daniel Fuchs <dfuchs@synyx.de>
2018-11-23 21:24:13 +0100

    refactor german stuff

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@ class KlimaSkill(MycroftSkill):
     def __init__(self):
         super(KlimaSkill, self).__init__(name="KlimaSkill")
 
-    @intent_handler(IntentBuilder("").require("get_temperature").require("room"))
+    @intent_handler(IntentBuilder("").require("getTemperature").require("room"))
     def handle_temperatur_intent(self, message):
         print(message.data)
         raum = message.data.get("room")
@@ -19,7 +19,7 @@ class KlimaSkill(MycroftSkill):
         api_url = "http://openhab-test.synyx.coffee:8080/rest/items/%s_Temperature_Current" % (raum)
         r = requests.get(api_url)
         temperature = r.json()["state"]
-        self.speak_dialog("get_temperature", data={"temperature": temperature})
+        self.speak_dialog("getTemperature", data={"temperature": temperature})
 
 def create_skill():
     return KlimaSkill()
